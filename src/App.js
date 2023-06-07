ddeafb71b00d19ac9d194cba23f92e9c41cea28e
ena.spremo <ena.spremo@base58.hr>
2023-06-07 12:08:32 +0200

    fixed app so that it works with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+  const [breakTime, setBreakTime] = React.useState(5 * 60);
+  const [sessionTime, setSessionTime] = React.useState(25 * 60);
+  const [timerOn, setTimerOn] = React.useState(false);
+  const [onBreak, setOnBreak] = React.useState(false);
+  const [breakAudio, setBreakAudio] = React.useState(new Audio("./breakTime.mp3"));
+
+  const playBreakSound = () => {
+    breakAudio.currentTime = 0;
+    breakAudio.play();
 }
 
+const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+        (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds)
+    );
+
+}
+
+const changeTime = (amount, type) => {
+    if (type == "break") {
+        if (breakTime <= 60 && amount < 0) return;
+        setBreakTime((prev) => prev + amount);
+    } else {
+        if (sessionTime <= 60 && amount < 0) return;
+        setSessionTime((prev) => prev + amount);
+        if (!timerOn) {
+            setDisplayTime(sessionTime + amount);
+        }
+    }
+}
+
+const controlTime = () => {
+    let second = 1000; // 1000ms is 1s
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    if (!timerOn) {
+        let interval = setInterval(() => {
+            date = new Date().getTime();
+            if (date > nextDate) {
+                setDisplayTime((prev) => {
+                    if (prev <= 0 && !onBreakVariable) {
+                        playBreakSound();
+                        onBreakVariable = true;
+                        setOnBreak(true);
+                        return breakTime;
+                    } else if (prev <= 0 && onBreakVariable) {
+                        playBreakSound();
+                        onBreakVariable = false;
+                        setOnBreak(false);
+                        return sessionTime;
+                    }
+                    return prev - 1;
+                });
+                nextDate += second;
+            }
+
+        }, 30);
+        localStorage.clear();
+        localStorage.setItem("interval-id", interval)//we want to update it every 30 miliseconds
+    }
+
+    if (timerOn) {
+        clearInterval(localStorage.getItem("interval-id"));
+    }
+    setTimerOn(!timerOn);
+};
+
+const resetTime = () => {
+    setDisplayTime(25 * 60);
+    setBreakTime(5 * 60);
+    setSessionTime(25 * 60);
+};
+
+return (
+    <div className="center-align">
+        <h1>25 + 5 Clock</h1>
+        <div className="dual-container">
+            <Length
+                title={"Break"}
+                changeTime={changeTime}
+                type={"break"}
+                time={breakTime}
+                formatTime={formatTime}
+            />
+            <Length
+                title={"Work"}
+                changeTime={changeTime}
+                type={"session"}
+                time={sessionTime}
+                formatTime={formatTime}
+            />
+        </div>
+        <h3>{onBreak ? "Break" : "Work"}</h3>
+        <div className="timer">
+            <h1>{formatTime(displayTime)}</h1>
+            <button className="btn-small #546e7a blue-grey darken-1"
+                onClick={controlTime}>
+                {timerOn ? (
+                    <i class="large material-icons">pause</i>
+                ) : (
+                    <i class="large material-icons">play_arrow</i>)}
+            </button>
+            <button className="btn-small #546e7a blue-grey darken-1"
+                onClick={resetTime}>
+                <i class="large material-icons">autorenew</i>
+            </button>
+        </div>
+    </div>
+);
+}
+
+
+function Length({ title, changeTime, type, time, formatTime }) {
+return (
+    <div>
+        <h3>{title}</h3>
+        <div className="time-sets">
+            <button className="btn-small #546e7a blue-grey darken-1"
+                onClick={() => changeTime(-60, type)}>
+                <i className="material-icons">arrow_downward</i>
+            </button>
+            <h3>{formatTime(time)}</h3>
+            <button className="btn-small #546e7a blue-grey darken-1"
+                onClick={() => changeTime(60, type)}>
+                <i className="material-icons">arrow_upward</i>
+            </button>
+        </div>
+    </div>
+
+);
+
+}
+//ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
